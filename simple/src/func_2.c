94f8d57ab7a3700d269b79c7efc62fd76f5c7f7d
Imdhh <ivan0ivanov0@mail.ru>
2014-09-16 19:21:49 +0300

    file func_2.c that will contain fakes has been added.

---
+++ simple/src/func_2.c
@@ -0,0 +1,16 @@
+/*
+ * func_2.c
+ *
+ *  Created on: Sep 16, 2014
+ *      Author: afi
+ */
+#include <stdio.h>
+
+int func_1_fake( void );
+
+int func_1_fake( void )
+{
+	printf( "kyky\n" );
+
+	return 0;
+}
