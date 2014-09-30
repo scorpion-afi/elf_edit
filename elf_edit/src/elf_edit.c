c21914f9e8ba01bdc781498005ce39b24d16b94b
Imdhh <ivan0ivanov0@mail.ru>
2014-09-30 21:09:37 +0300

    now we have changed one byte in elf file(simple) (func_1 code), now func_1 increases returned value by two, not by one.

--- elf_edit/src/elf_edit.c
+++ elf_edit/src/elf_edit.c
@@ -23,10 +23,12 @@ int main( void )
 	off_t size = 0;
 	off_t temp = 0;
 	ssize_t num_of_read_byte = 0;
+	ssize_t num_of_wrote_bytes = 0;
 
 	Elf64_Ehdr* p_elf_header = NULL;
 	Elf64_Shdr* p_sec_header_table_entry = NULL;
 	Elf64_Sym*  p_sym_table_entry = NULL;
+	Elf64_Off func_1_offset = 0;
 
 	fd = open( "/home/afi/workspace_elf_injection/simple/Debug/simple", O_RDWR );
 	if( fd == -1 )
@@ -82,6 +84,21 @@ int main( void )
 
 	printf( "%c%c%c\n", p_elf_header->e_ident[1], p_elf_header->e_ident[2], p_elf_header->e_ident[3] );
 
+	//--------------------------- .symtab string ----------------------------------------------
+
+	// obtain pointer to first(0) section header table entry (to first(0) section header)
+	p_sec_header_table_entry = (Elf64_Shdr*)( p_elf_content + p_elf_header->e_shoff );
+
+	//shift to .symtab section
+	p_sec_header_table_entry += 35;
+
+	// get pointer to first string in .symtab section
+	char* tempik = (char*)( p_elf_content + p_sec_header_table_entry->sh_offset ) + 1;
+
+	printf( "kk: %s\n", tempik );
+
+	//---------------------------------------------------------------------------------
+
 	// obtain pointer to first(0) section header table entry (to first(0) section header)
 	p_sec_header_table_entry = (Elf64_Shdr*)( p_elf_content + p_elf_header->e_shoff );
 
@@ -91,22 +108,56 @@ int main( void )
 
 	// obtain pointer to section .symtab (to first(0) symtab entry)
 	p_sym_table_entry = (Elf64_Sym*)( p_elf_content + p_sec_header_table_entry->sh_offset );
+	p_sym_table_entry += 44; // jump to func_1
 
-	//----------------
+	func_1_offset = (Elf64_Off)( p_sym_table_entry->st_value - 0x400000 );
+
+	//---------------------------------------------------------------------------------
 
 	// obtain pointer to first(0) section header table entry (to first(0) section header)
 	p_sec_header_table_entry = (Elf64_Shdr*)( p_elf_content + p_elf_header->e_shoff );
-	p_sec_header_table_entry += 33;
-	printf( "kk: %s\n", (char*)( p_elf_content + p_sec_header_table_entry->sh_offset ) );
 
+	//shift to .text section
+	p_sec_header_table_entry += 13;
 
-	Elf64_Off offset =  p_elf_header->e_shstrndx * p_elf_header->e_shentsize + p_elf_header->e_shoff;
+	// get pointer to first byte of executable code in .text section
+	// this is _start function code (from gcc or glibc.so)
+	tempik = (char*)( p_elf_content + p_sec_header_table_entry->sh_offset );
 
-	/*
-	for(;;)
+	// this is func_1 function code
+	tempik = p_elf_content + func_1_offset;
+
+	// change a++ to a += 2;
+	tempik += 10;
+
+	*tempik = 2;
+
+	//---------------------------------------------------------------------------------
+
+	close( fd );
+	remove( "/home/afi/workspace_elf_injection/simple/Debug/simple" );
+
+	fd = open( "/home/afi/workspace_elf_injection/simple/Debug/simple", O_CREAT | O_RDWR );
+	if( fd == -1 )
 	{
-		if(p_sym_table_entry->st_name)
-	}*/
+		perror( NULL );
+		return EXIT_FAILURE;
+	}
+
+	printf( "fd = %d\n", fd );
+
+	num_of_wrote_bytes = write( fd, p_elf_content, size );
+	if( num_of_wrote_bytes != size )
+	{
+		perror( NULL );
+		return EXIT_FAILURE;
+	}
+
+	close( fd );
+
+	// 0x602010 - p_elf_content
+	// 0x6025a2 - func_1
+	// 0x6025b3 - func_2
 
 	return EXIT_SUCCESS;
 }
